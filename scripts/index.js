790fc9ce8601558c8ba67793eda610411ab6be40
AlexeyYakovec <yakovec8952@gmail.com>
2024-03-06 17:25:48 +0500

    final stage of the mesto project

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,43 @@
-// buttons
+// Находим кнопки
 const addButton = document.querySelector(".profile__add-button");
 const editButton = document.querySelector(".profile__edit");
 
-// Container and photo template (Контейнер и шаблон для фото)
+// Контейнер и шаблон для фото
 const photoContainer = document.querySelector(".photo-grid");
 const photoTemplate = document.querySelector("#photo-grid-template");
 
-// popup
+// Попапы
 const popupProfile = document.querySelector(".popup_type_profile");
 const popupPhoto = document.querySelector(".popup_type_photo");
-const closeProfileButton = document.querySelector(".popup_close");
-const closePhotoButton = document.querySelector(".popup_close");
+const closeProfileButton = popupProfile.querySelector(".popup__close");
+const closePhotoButton = popupPhoto.querySelector(".popup__close");
 
-const popupModal = document.querySelector(".popup_type_modal");
-const modalClose = popupModal.querySelector(".popup__close");
-const modalSrc = popupModal.querySelector(".popup__pic");
-const modalTitle = popupModal.querySelector(".popup__pic-caption");
-
-// profile form
+// Профайл
 const formProfile = document.querySelector(".form_type_profile");
 const nameInput = document.querySelector(".form__input_type_name");
 const jobInput = document.querySelector(".form__input_type_job");
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
 
-// photo
+// Фото
 const formPhoto = document.querySelector(".form_type_photo");
 const inputList = Array.from(formPhoto.querySelectorAll(".form__input"));
 const buttonElement = formPhoto.querySelector(".form__submit");
 const photoTitleInput = document.querySelector(".form__input_type_title");
 const photoLinkInput = document.querySelector(".form__input_type_src");
 
+const popupModal = document.querySelector(".popup_type_modal");
+const modalClose = popupModal.querySelector(".popup__close");
+const modalSrc = popupModal.querySelector(".popup__pic");
+const modalTitle = popupModal.querySelector(".popup__pic-caption");
+
+// Добавление фото/мест
 function addPhoto(name, link) {
   const newPhoto = photoTemplate.content
     .querySelector(".photo-grid__item")
     .cloneNode(true);
-
   const photoSrc = newPhoto.querySelector(".photo-grid__pic");
   const photoTitle = newPhoto.querySelector(".photo-grid__title");
-
   const deleteButton = newPhoto.querySelector(".photo-grid__delete");
   const likeButton = newPhoto.querySelector(".photo-grid__like");
 
@@ -56,8 +55,8 @@ function addPhoto(name, link) {
 }
 
 // Подгружаем первые карточки (из initial-cards.js)
-initialCards.forEach((item) => {
-  const newCard = addPhoto(item.name, item.link);
+initialCards.forEach(function (item) {
+  const newCard = addPhoto(item["name"], item["link"]);
   photoContainer.append(newCard);
 });
 
@@ -79,6 +78,96 @@ function handleModal(name, link) {
   modalSrc.src = link;
 }
 
+// Обработчик «отправки» формы
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+  // Получите значение полей jobInput и nameInput из свойства value
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+
+  // Закрываем попап
+  closePopup(popupProfile);
+}
+
+// Форма для фото
+function handlePhotoFormSubmit(evt) {
+  evt.preventDefault();
+  photoContainer.prepend(addPhoto(photoTitleInput.value, photoLinkInput.value));
+
+  // Очищаем поля
+  formPhoto.reset();
+
+  closePopup(popupPhoto);
+}
+
+function editProfile() {
+  openPopup(popupProfile);
+
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
 function openPopup(popupName) {
   popupName.classList.add("popup_opened");
+
+  // добавляем обработчики закрытия по Escape и клику на overlay
+  document.addEventListener("keydown", handleHotkey);
+  document.addEventListener("mousedown", handleOverlayClick);
 }
+
+function closePopup(popupName) {
+  popupName.classList.remove("popup_opened");
+
+  // удаляем обработчики закрытия по Escape и клику на overlay
+  document.removeEventListener("keydown", handleHotkey);
+  document.removeEventListener("mousedown", handleOverlayClick);
+}
+
+function handleHotkey(evt) {
+  // проверяем есть ли открытый попап и только тогда закрываем
+  const activePopup = document.querySelector(".popup_opened");
+  if (activePopup && evt.key === "Escape") {
+    closePopup(activePopup);
+  }
+}
+
+function handleOverlayClick(evt) {
+  const activePopup = document.querySelector(".popup_opened");
+
+  if (activePopup && evt.target === activePopup) {
+    closePopup(activePopup);
+  }
+}
+
+// включение валидации вызовом enableValidation
+// все настройки передаются при вызове
+const config = {
+  formSelector: ".form",
+  inputSelector: ".form__input",
+  submitButtonSelector: ".form__submit",
+  inactiveButtonClass: "form__submit_disabled",
+  inputErrorClass: "form__input_type_error",
+  errorClass: "form__input-error_active",
+};
+
+// enableValidation(config);
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formProfile.addEventListener("submit", handleProfileFormSubmit);
+formPhoto.addEventListener("submit", handlePhotoFormSubmit);
+
+editButton.addEventListener("click", editProfile);
+addButton.addEventListener("click", () => {
+  openPopup(popupPhoto);
+});
+closeProfileButton.addEventListener("click", () => {
+  closePopup(popupProfile);
+});
+closePhotoButton.addEventListener("click", () => {
+  closePopup(popupPhoto);
+});
+modalClose.addEventListener("click", () => {
+  closePopup(popupModal);
+});
